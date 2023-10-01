db829a97af119e28e0fa689345dae5b33a91fdd1
mustafamhmod95@gmail.com <mustafamhmod95@gmail.com>
2023-10-01 21:36:20 +0300

    small Fix in app.moddule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewComponent } from './new/new.component';
 import { ComonetntAnythingComponent } from './comonetnt-anything/comonetnt-anything.component';
-import { MostafaIbrahimComponent } from './components/mostafa-ibrahim/mostafa-ibrahim.component';
-import { MostafaIbrahimBranchComponent } from './components/mostafa-ibrahim-branch/mostafa-ibrahim-branch.component';
 import { SeventhTrialComponent } from './components/seventh-trial/seventh-trial.component';
 
 @NgModule({
@@ -14,15 +11,10 @@ import { SeventhTrialComponent } from './components/seventh-trial/seventh-trial.
     AppComponent,
     NewComponent,
     ComonetntAnythingComponent,
-    MostafaIbrahimComponent,
-    MostafaIbrahimBranchComponent,
-    SeventhTrialComponent
+    SeventhTrialComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
